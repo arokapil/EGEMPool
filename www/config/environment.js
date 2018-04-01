cd1dbeaf80ae40f2af2862aa49d03e7ea4bc070f
MakeMoneyOz <33920888+MakeMoneyOz@users.noreply.github.com>
2018-04-01 10:58:02 +1000

    Change to EGEM block time 13s

--- www/config/environment.js
+++ www/config/environment.js
@@ -30,7 +30,7 @@ module.exports = function(environment) {
       PayoutThreshold: '0.5 EGEM',
 
       // For network hashrate (change for your favourite fork)
-      BlockTime: 14.4
+      BlockTime: 13.0 // EGEM BLOCK TIME 
     }
   };
 
